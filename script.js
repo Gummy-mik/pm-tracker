5a7cc6dc723ad723aeb68b217fe6ee582aa3c10a
Gummy-mik <mlcolisao@gmail.com>
2026-02-12 10:27:18 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let calculateBtn = document.getElementById("calculateBtn");
+calculateBtn.addEventListener("click", function() {
+  let totalSites = document.getElementById("totalSites").value;
+  let visitsPerSite = document.getElementById("visitsPerSite").vale;
+  let visitsSoFar = document.getElementById("visitsSoFar").value;
+  
+  console.log(totalsSites, visitsPerSites, visitsSoFar); });
