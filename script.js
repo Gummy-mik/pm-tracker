7782b1f8ebee74ee99b592549d0039083147ec36
Gummy-mik <mlcolisao@gmail.com>
2026-02-12 18:50:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let calculateBtn = document.getElementById("calculateBtn");
 
 calculateBtn.addEventListener("click", function() {
+  
+  let RCBCBankSites = Number(document.getElementById("RCBCBankSites).value);
+  let PLC = Number(document.getElementById("PLC").value);
+  let Offsites = Number(document.getElementById("Offsites").value);
   let totalSites = Number(document.getElementById("totalSites").value);
   let visitsPerSite = Number(document.getElementById("visitsPerSite").value);
   let visitsSoFar = Number(document.getElementById("visitsSoFar").value);
-
+  
+  let totalSites = RCBCBankSites + PLC + Offsites;
   let totalRequired = totalSites * visitsPerSite;
   let remaining = totalRequired - visitsSoFar;
   
